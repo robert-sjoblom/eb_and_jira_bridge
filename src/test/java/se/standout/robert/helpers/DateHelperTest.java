e689af823dcefe7a15e5931b4ec56b4b35e5725e
Robert Sjöblom <robert.sjoblom@gmail.com>
2020-08-28 21:58:09 +0200

    Add test file for DateHelper

---
+++ src/test/java/se/standout/robert/helpers/DateHelperTest.java
@@ -0,0 +1,23 @@
+package se.standout.robert.helpers;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple DateHelper.
+ *
+ * public static getCurrentWeek()
+ *
+ * public static getPreviousDaysInWeek()
+ *
+ */
+public class DateHelperTest {
+  /**
+   * Rigorous Test :-)
+   */
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}
